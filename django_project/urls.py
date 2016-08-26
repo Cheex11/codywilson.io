7ec099279769001793a36ac1738e851f23ae6901
Cody Wilson <Cheex11@gmail.com>
2016-08-25 23:12:46 -0400

    first commit

---
+++ django_project/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls.static import  static
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
